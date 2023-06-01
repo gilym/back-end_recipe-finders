0fc95f90eadde5f33dc8c2aa07c41c6a37610258
Gilang Yoenal Marinta <gilym46@gmail.com>
2023-06-01 17:04:40 +0700

    commit

--- routes/foodrouter.js
+++ routes/foodrouter.js
@@ -47,17 +47,39 @@ foodrouter.delete("/food/:id", (req, res) => {
     });
 });
 
-foodrouter.post('/food', multer.single('img'), imgUpload.uploadToGcs, (req, res) => {
 
+foodrouter.post('/food', multer.single('img'), imgUpload.uploadToGcs, (req, res) => {
     const { name, category, description, ingredients } = req.body;
     const imageUrl = req.file ? req.file.cloudStoragePublicUrl : '';
-  
     const query = 'INSERT INTO food (name, category, description, ingredients, img) VALUES (?, ?, ?, ?, ?)';
     connection.query(query, [name, category, description, ingredients, imageUrl], (err, result) => {
       if (err) {
         res.status(500).send({ message: err.sqlMessage });
       } else {
         res.status(201).send({ message: 'Food inserted successfully', insertId: result.insertId });
+      }
+    });
+  });
+
+  foodrouter.put('/food/:id', multer.single('img'), imgUpload.uploadToGcs, (req, res) => {
+    const id = req.params.id;
+    const { name, category, description, ingredients } = req.body;
+    let imageUrl = '';
+  
+    if (req.file && req.file.cloudStoragePublicUrl) {
+      imageUrl = req.file.cloudStoragePublicUrl;
+    }
+  
+    const query = 'UPDATE food SET name = ?, category = ?, description = ?,ingredients = ?, img = ? WHERE id = ?';
+    connection.query(query, [name, category, description, ingredients, imageUrl, id], (err, result) => {
+      if (err) {
+        res.status(500).send({ message: err.sqlMessage });
+      } else {
+        if (result.affectedRows === 0) {
+          res.status(404).send({ message: 'Food not found' });
+        } else {
+          res.status(200).send({ message: 'Food updated successfully' });
+        }
       }
     });
   });

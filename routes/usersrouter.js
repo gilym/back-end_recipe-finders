59fafa5ad1e3e5088b5c1e802097846e85b3087a
Gilang Yoenal Marinta <gilym46@gmail.com>
2023-06-01 16:56:09 +0700

    commit

--- routes/usersrouter.js
+++ routes/usersrouter.js
@@ -60,6 +60,51 @@ usersrouter.post('/users', multer.single('img'), imgUpload.uploadToGcs, (req, re
       }
     });
   });
+
+  usersrouter.put('/edit_Pass/:id', (req, res) => {
+    const id = req.params.id;
+    const { password } = req.body;
+    const query = 'UPDATE users SET password = ? WHERE id = ?';
+    connection.query(query, [password, id], (err, result) => {
+      if (err) {
+        res.status(500).send({ message: err.sqlMessage });
+      } else {
+        if (result.affectedRows === 0) {
+          res.status(404).send({ message: 'User not found' });
+        } else {
+          res.status(200).send({ message: 'User password updated successfully' });
+        }
+      }
+    });
+  });
+
+  usersrouter.put('/users/:id', multer.single('img'), imgUpload.uploadToGcs, (req, res) => {
+    const id = req.params.id;
+    const { username, preferences } = req.body;
+    let imageUrl = '';
+  
+    if (req.file && req.file.cloudStoragePublicUrl) {
+      imageUrl = req.file.cloudStoragePublicUrl;
+    }
+  
+    const query = 'UPDATE users SET username = ?, preferences = ?, img = ? WHERE id = ?';
+    connection.query(query, [username, preferences, imageUrl, id], (err, result) => {
+      if (err) {
+        res.status(500).send({ message: err.sqlMessage });
+      } else {
+        if (result.affectedRows === 0) {
+          res.status(404).send({ message: 'Users not found' });
+        } else {
+          res.status(200).send({ message: 'Users updated successfully' });
+        }
+      }
+    });
+  });
+
+
+  
+
+  
   
 
 

e59f71c2735860d063d9d7394fea2dda951fc3b9
Gilang Yoenal Marinta <gilym46@gmail.com>
2023-06-03 12:50:14 +0700

    commit

--- app.js
+++ app.js
@@ -1,26 +1,29 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors'); // Import middleware CORS
 
-const preferences = require('./routes/preferencesrouter')
-const food = require('./routes/foodrouter')
-const user = require('./routes/usersrouter')
-const history = require('./routes/historyrouter')
+const preferences = require('./routes/preferencesrouter');
+const food = require('./routes/foodrouter');
+const user = require('./routes/usersrouter');
+const history = require('./routes/historyrouter');
 
 app.use(bodyParser.json()); // Middleware untuk mem-parsing body request dalam format JSON
-app.use(bodyParser.urlencoded({extended: true})); // Middleware untuk mem-parsing body request dalam format application/x-www-form-urlencoded
-app.use(preferences)
-app.use(food)
+app.use(bodyParser.urlencoded({ extended: true })); // Middleware untuk mem-parsing body request dalam format application/x-www-form-urlencoded
 
-app.use(user)
-app.use(history)
+app.use(cors()); // Tambahkan middleware CORS sebelum menggunakan router
+
+app.use(preferences);
+app.use(food);
+app.use(user);
+app.use(history);
 
 app.get("/", (req, res) => {
-    console.log("Response success")
-    res.send("Response Success!")
-})
+    console.log("Response success");
+    res.send("Response Success!");
+});
 
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
-    console.log("Server is up and listening on " + PORT)
-})
+    console.log("Server is up and listening on " + PORT);
+});
